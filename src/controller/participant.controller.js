a2ecff10644d7ea34e7ba9c6143d5fbc98777f4d
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-09 15:17:39 +0530

    create participant route changes

--- src/controller/participant.controller.js
+++ src/controller/participant.controller.js
@@ -1,82 +1,88 @@
 const Participant = require("../models/participant.model");
-const Contest=require("../models/contest.model");
+const Contest = require("../models/contest.model");
 const moment = require("moment");
 
-async function createParticipant(req,res){
-    const{userId,contestId}=req.body;
-    //5d6ede6a0ba62570afcedd3a
-    try {
-        const createParticipant=await Participant.create({
-            userId,
-            contestId
-        });
-        return res.status(201).send(createParticipant);
-    } catch (error) {
-        return res.status(400).send(err.message);
-    }
+async function createParticipant(req, res) {
+  const { userId, contestId } = req.body;
+  //5d6ede6a0ba62570afcedd3a
+  try {
+    const createParticipant = await Participant.create({
+      userId,
+      contestId,
+    });
+    return res.status(201).send(createParticipant);
+  } catch (error) {
+    return res.status(400).send(err.message);
+  }
 }
 
-async function attemptQuestion(req,res){
-    const{questionId,attempted,userId,contestId}=req.body;
-    try {
-        filter={$and:[{userId:userId},{contestId:contestId}],"questions.questionId":questionId}
-        update={"$set":{"questions.$.attempted":attempted}}
-        const updateAttempted=await Participant.updateOne(
-            filter,update
-        )
-        return res.send({msg:"updated attempted question"});
-    } catch (error) {
-        return res.status(400).send(err.message);
-    }
+async function attemptQuestion(req, res) {
+  const { questionId, attempted, userId, contestId } = req.body;
+  try {
+    filter = {
+      $and: [{ userId: userId }, { contestId: contestId }],
+      "questions.questionId": questionId,
+    };
+    update = { $set: { "questions.$.attempted": attempted } };
+    const updateAttempted = await Participant.updateOne(filter, update);
+    return res.send({ msg: "updated attempted question" });
+  } catch (error) {
+    return res.status(400).send(err.message);
+  }
 }
 
-async function bookmarkQuestion(req,res){
-    const{questionId,userId,contestId,bookmark}=req.body;
-    try {
-        filter={$and:[{userId:userId},{contestId:contestId}],"questions.questionId":questionId}
-        update={"$set":{"questions.$.bookmark":bookmark}}
-        const updateAttempted=await Participant.updateOne(
-            filter,update
-        )
-        return res.status(200).send({msg:"bookmarked question"});
-    } catch (error) {
-        return res.status(400).send(err.message);
-    }
+async function bookmarkQuestion(req, res) {
+  const { questionId, userId, contestId, bookmark } = req.body;
+  try {
+    filter = {
+      $and: [{ userId: userId }, { contestId: contestId }],
+      "questions.questionId": questionId,
+    };
+    update = { $set: { "questions.$.bookmark": bookmark } };
+    const updateAttempted = await Participant.updateOne(filter, update);
+    return res.status(200).send({ msg: "bookmarked question" });
+  } catch (error) {
+    return res.status(400).send(err.message);
+  }
 }
 
-async function clearQuestion(req,res){
-    const{questionId,userId,contestId}=req.body;
-    try {
-        filter={$and:[{userId:userId},{contestId:contestId}],"questions.questionId":questionId};
-        update={"$set":{"questions.$.attempted":null}};
-        const updateAttempted=await Participant.updateOne(
-            filter,update
-        )
-        return res.status(200).send({msg:"question selection cleared"});
-    } catch (error) {
-        return res.status(400).send(err.message);
-    }
+async function clearQuestion(req, res) {
+  const { questionId, userId, contestId } = req.body;
+  try {
+    filter = {
+      $and: [{ userId: userId }, { contestId: contestId }],
+      "questions.questionId": questionId,
+    };
+    update = { $set: { "questions.$.attempted": null } };
+    const updateAttempted = await Participant.updateOne(filter, update);
+    return res.status(200).send({ msg: "question selection cleared" });
+  } catch (error) {
+    return res.status(400).send(err.message);
+  }
 }
 
-async function submitTest(req,res){
-    const{userId,contestId}=req.body;
-    try {
-        const findContest = await Contest.findById(contestId);
-        var startTime=moment(findContest.started);
-        var now=moment(new Date());
-        var timeTaken=moment.duration(startTime.diff(now));
-        var timeTakenSeconds = timeTaken.asSeconds();
+async function submitTest(req, res) {
+  const { userId, contestId } = req.body;
+  try {
+    const findContest = await Contest.findById(contestId);
+    var startTime = moment(findContest.started);
+    var now = moment(new Date());
+    var timeTaken = moment.duration(startTime.diff(now));
+    var timeTakenSeconds = timeTaken.asSeconds();
 
-        filter={$and:[{userId:userId},{contestId:contestId}]};
-        update={"timeTaken":timeTakenSeconds};
-        const submitTest=await Participant.updateOne(
-            filter,update
-        )
-        return res.status(200).send({msg:"Test Submitted!"});
-    } catch (error) {
-        return res.status(400).send(err.message);
-    }
-    
+    filter = { $and: [{ userId: userId }, { contestId: contestId }] };
+    update = { timeTaken: timeTakenSeconds };
+    const submitTest = await Participant.updateOne(filter, update);
+    return res.status(200).send({ msg: "Test Submitted!" });
+  } catch (error) {
+    return res.status(400).send(err.message);
+  }
 }
 
-module.exports={attemptQuestion,createParticipant,bookmarkQuestion,clearQuestion,submitTest}
+module.exports = {
+  attemptQuestion,
+  createParticipant,
+  bookmarkQuestion,
+  clearQuestion,
+  submitTest,
+};
--- src/routers/participant.router.js
+++ src/routers/participant.router.js
@@ -1,10 +1,16 @@
-const { attemptQuestion,createParticipant,bookmarkQuestion,clearQuestion,submitTest} = require("../controller/participant.controller");
+const {
+  attemptQuestion,
+  createParticipant,
+  bookmarkQuestion,
+  clearQuestion,
+  submitTest,
+} = require("../controller/participant.controller");
 
 const participantRouter = require("express").Router();
 participantRouter.get("/");
-participantRouter.post("/createparticipant",createParticipant);
-participantRouter.put("/attempted",attemptQuestion);
-participantRouter.put("/bookmark",bookmarkQuestion);
-participantRouter.put("/clearattempted",clearQuestion);
-participantRouter.put("/submit",submitTest);
+participantRouter.post("/", createParticipant);
+participantRouter.put("/attempted", attemptQuestion);
+participantRouter.put("/bookmark", bookmarkQuestion);
+participantRouter.put("/clearattempted", clearQuestion);
+participantRouter.put("/submit", submitTest);
 module.exports = participantRouter;
